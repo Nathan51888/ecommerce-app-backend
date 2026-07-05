86a11e1cf281d70374afe3c876a45ef03e343dc8
Nathan51888 <NathanLiang99@outlook.com>
2026-07-05 12:56:12 +1200

    Added base integration test class

--- EcommerceApp.Test/Abstractions/BaseIntegrationTest.cs
+++ EcommerceApp.Test/Abstractions/BaseIntegrationTest.cs
@@ -1,6 +1,29 @@
+using Meziantou.Extensions.Logging.Xunit;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
 namespace EcommerceApp.Test.Abstractions;
 
-public class BaseIntegrationTest
+public class BaseIntegrationTest : IClassFixture<IntegrationTestWebAppFactory>
 {
-    
+    private readonly ITestOutputHelper _testOutputHelper;
+    protected HttpClient HttpClient { get; init; }
+
+    protected BaseIntegrationTest(IntegrationTestWebAppFactory factory, ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+        var factoryWithLogger = factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureLogging(x =>
+            {
+                x.ClearProviders();
+                x.SetMinimumLevel(LogLevel.Debug);
+
+                x.Services.AddSingleton<ILoggerProvider>(new XUnitLoggerProvider(_testOutputHelper));
+            });
+        });
+        HttpClient = factoryWithLogger.CreateClient();
+    }
 }
--- EcommerceApp.Test/Abstractions/IntegrationTestWebAppFactory.cs
+++ EcommerceApp.Test/Abstractions/IntegrationTestWebAppFactory.cs
@@ -1,6 +1,49 @@
+using EcommerceApp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Testcontainers.PostgreSql;
+
 namespace EcommerceApp.Test.Abstractions;
 
-public class IntegrationTestWebAppFactory
+public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
 {
-    
+    private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+        .WithImage("postgres:latest")
+        .Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(s => s.ServiceType == typeof(DbContextOptions<AppDbContext>));
+            if (descriptor is not null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options
+                    .UseNpgsql(_dbContainer.GetConnectionString())
+                    .UseSnakeCaseNamingConvention();
+            });
+        });
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _dbContainer.StartAsync();
+        var scope = Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        await dbContext.Database.MigrateAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _dbContainer.DisposeAsync();
+    }
 }
