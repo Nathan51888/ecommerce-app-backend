37e67ca7dc1b86ec0799e021c7b82d2e57ee583a
Nathan51888 <NathanLiang99@outlook.com>
2026-07-16 18:49:54 +1200

    Added Seq logging

--- EcommerceApp.Admin/Program.cs
+++ EcommerceApp.Admin/Program.cs
@@ -103,6 +103,26 @@ try
         app.MapScalarApiReference();
     }
 
+    app.UseSerilogRequestLogging(options =>
+    {
+        options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+        {
+            diagnosticContext.Set("RequestHost", httpContext.Request.Host.Value);
+            diagnosticContext.Set("UserAgent", httpContext.Request.Headers.UserAgent.ToString());
+        };
+
+        // Exclude health check endpoints from request logs
+        options.GetLevel = (httpContext, elapsed, ex) =>
+        {
+            if (httpContext.Request.Path.StartsWithSegments("/health"))
+                return Serilog.Events.LogEventLevel.Verbose;
+
+            return elapsed > 500
+                ? Serilog.Events.LogEventLevel.Warning
+                : Serilog.Events.LogEventLevel.Information;
+        };
+    });
+
     app.UseHttpsRedirection();
 
     app.UseAuthentication();
